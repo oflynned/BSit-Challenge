5284d1d62e99498481a8dcd4a4ba962df575caa6
Edmond O'Flynn <oflynned@tcd.ie>
2019-07-24 15:20:19 +0200

    add route test cases

--- test/routes/championRoute.test.js
+++ test/routes/championRoute.test.js
@@ -36,32 +36,54 @@ describe(`${url} endpoint`, () => {
           done();
         });
       });
-    });
-
-    describe("/:championId", () => {
-      describe("should return 200", () => {
-        test("when accessing an existing champion", async done => {
-          const { body, status } = await getResource(app, url + "/" + championId);
-          expect(status).toEqual(200);
-          expect(Array.isArray(body)).toBe(false);
-          expect(body.hasOwnProperty("name")).toBe(true);
-          expect(body.id).toEqual(championId);
-          done();
-        });
-      });
-
-      describe("should return 404", () => {
-        test("when accessing a non-existent champion", async done => {
+      describe("should return 400", () => {
+        test("when passing < -1 for limit", async done => {
           try {
-            await getResource(app, url + "/does-not-exist");
-            done("did not throw http 404");
+            await getResource(app, url + "?limit=-10&offset=0");
+            done("did not throw http 400");
           } catch ({ response }) {
-            expect(response.status).toEqual(404);
-            expect(response.body.error).toEqual("champion_not_found");
+            expect(response.status).toEqual(400);
+            expect(response.body.error).toEqual("bad_query_parameter");
+            done();
+          }
+        });
+
+        test("when passing < -1 for offset", async done => {
+          try {
+            await getResource(app, url + "?limit=10&offset=-10");
+            done("did not throw http 400");
+          } catch ({ response }) {
+            expect(response.status).toEqual(400);
+            expect(response.body.error).toEqual("bad_query_parameter");
             done();
           }
         });
       });
+    });
+  });
+});
+describe("/:championId", () => {
+  describe("should return 200", () => {
+    test("when accessing an existing champion", async done => {
+      const { body, status } = await getResource(app, url + "/" + championId);
+      expect(status).toEqual(200);
+      expect(Array.isArray(body)).toBe(false);
+      expect(body.hasOwnProperty("name")).toBe(true);
+      expect(body.id).toEqual(championId);
+      done();
+    });
+  });
+
+  describe("should return 404", () => {
+    test("when accessing a non-existent champion", async done => {
+      try {
+        await getResource(app, url + "/does-not-exist");
+        done("did not throw http 404");
+      } catch ({ response }) {
+        expect(response.status).toEqual(404);
+        expect(response.body.error).toEqual("champion_not_found");
+        done();
+      }
     });
   });
 });
