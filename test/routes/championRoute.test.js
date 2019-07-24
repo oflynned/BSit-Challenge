a3d6957d7cff5051e2fb1e05cc0a34a99d563e0c
Edmond O'Flynn <oflynned@tcd.ie>
2019-07-24 15:18:29 +0200

    create factory

---
+++ test/routes/championRoute.test.js
@@ -0,0 +1,67 @@
+import { dropDb } from "../helpers/dbHelper";
+import { getResource } from "../helpers/requestHelper";
+import { seedChampions } from "../../seeds/seedChampions";
+
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+
+let app;
+const url = "/api/champions";
+const championId = "aatrox";
+
+describe(`${url} endpoint`, () => {
+  beforeAll(async () => {
+    await dropDb();
+    await seedChampions();
+    app = require("../../app");
+    chai.use(chaiHttp);
+  });
+
+  afterAll(async () => dropDb());
+
+  describe("GET", () => {
+    describe("/", () => {
+      describe("should return 200", () => {
+        test("when accessing the bare endpoint", async done => {
+          const { body, status } = await getResource(app, url);
+          expect(status).toEqual(200);
+          expect(Array.isArray(body)).toBe(true);
+          done();
+        });
+
+        test("when accessing with a query parameter", async done => {
+          const { body, status } = await getResource(app, `${url}?limit=10&offset=0`);
+          expect(status).toEqual(200);
+          expect(Array.isArray(body)).toBe(true);
+          done();
+        });
+      });
+    });
+
+    describe("/:championId", () => {
+      describe("should return 200", () => {
+        test("when accessing an existing champion", async done => {
+          const { body, status } = await getResource(app, url + "/" + championId);
+          expect(status).toEqual(200);
+          expect(Array.isArray(body)).toBe(false);
+          expect(body.hasOwnProperty("name")).toBe(true);
+          expect(body.id).toEqual(championId);
+          done();
+        });
+      });
+
+      describe("should return 404", () => {
+        test("when accessing a non-existent champion", async done => {
+          try {
+            await getResource(app, url + "/does-not-exist");
+            done("did not throw http 404");
+          } catch ({ response }) {
+            expect(response.status).toEqual(404);
+            expect(response.body.error).toEqual("champion_not_found");
+            done();
+          }
+        });
+      });
+    });
+  });
+});
